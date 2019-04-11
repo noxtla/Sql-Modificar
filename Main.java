bb1b46893f6dae31583dbf3f08833abdb9f1b026
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-04-11 12:13:46 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
-
+public void btnUpdate () {
+		
+		Connection laConexionInsertar = null;
+		
+		try {
+			laConexionInsertar = getConnection();
+			ps = laConexionInsertar.prepareStatement("update persona set clave=?,nombre=? where idPersona=?");
+			ps.setString(1, cajaClave.getText());
+			
+			///Esto es nuevo
+			ps.setInt(8, Integer.parseInt(cajaID.getTExt() ));
+			
+			///Aqui solo indicamos. No ejecutamos nada
+			
+			int resultado = ps.executeUpdate(); ///Ejecutamos la modificacion 
+			
+			if (resultado>0) {
+			///Exito
+		}
+			else {
+				///No exito
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
